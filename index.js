9008608d71ea6dd110c7d6c75e1a0f4b238ef332
Abhinaw Raj <abhinawraj15@gmail.com>
2025-05-16 23:26:07 +0530

    arrays concept.

--- index.js
+++ index.js
@@ -58,7 +58,54 @@ console.log("Hello, World!");
 // const nums = [1, 2, 3, 4, 5];
 // console.log(Math.min(...nums));
 
-const obj = { name: "John", age: 30, city: "New York" };
-const { name, ...rest } = obj;
-console.log(name);
-console.log(rest);
+// const obj = { name: "John", age: 30, city: "New York" };
+// const { name, ...rest } = obj;
+// console.log(name);
+// console.log(rest);
+
+// const nowDate = new Date();
+// console.log(nowDate);
+// console.log(nowDate.getTime());
+// console.log(nowDate.getFullYear());
+// console.log(nowDate.getUTCFullYear());
+// console.log(nowDate.toLocaleString("en-GB")); //can give options like hour12, weekday, year,month
+// console.log(nowDate.toLocaleDateString("en-GB")); //can give options like hour12, weekday, year,month
+// // console.log(nowDate.toLocaleTimeString("en-GB")); //can give options like hour12, weekday, year,month
+// console.log(nowDate.nowDate);
+// console.log(nowDate.Date);
+
+//Arryas
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(arr);
+//console.log(arr2);
+
+// console.log(arr[0]); //access
+// console.log(arr.length); //length
+// console.log(arr[arr.length - 1]); //last element
+
+// console.log(arr.push(14)); //add to end
+// console.log(arr.pop()); //remove last element
+// console.log(arr.shift()); //remove first element and the array length decreases
+// console.log(arr);
+//console.log(arr.unshift(98)); //add to start - the array length increases
+//console.log(arr);
+
+// console.log(arr.slice(2, 5)); //slice - returns a new array -startIndex nclusive and endIndex exclusive - original array is not affected as it returns a copy of it
+// console.log(arr);
+
+// console.log(arr.splice(2, 5)); //slice - returns a new array -startIndex nclusive deletes the provided count in second argument - original array is affected
+// console.log(arr);
+
+// const arr3 = [5, 2, 7, 3, 6];
+// console.log(arr3.sort()); //sort - sorts the array in place and returns the sorted array - original array is affected
+// console.log(arr3.sort((a, b) => b - a)); //descending order
+
+// const arr4 = ["apple", "banana", "cherry"];
+// console.log(arr4.join("-")); //join - joins the array elements into a string(default is comma) - original array is not affected, returns a string - Real world case - making csv file
+
+// const nested = [1, [2, 3], [4, [5]]];
+// console.log(nested.flat(1)); // [1, 2, 3, 4, 5]
+// const names = ["Alice", "Bob", "Alice", "Eve"];
+// const dups = [...new Set(names)];
+// console.log(dups); // [ 'Alice', 'Bob', 'Eve' ]
